89f819b1247e2f72b73a3fc674cdda7a5cf11918
Sonnumber5 <157999365+Sonnumber5@users.noreply.github.com>
2024-08-07 22:28:18 -0400

    Add files via upload

---
+++ Example 2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Example_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int StartingNumber = 6;
+            Console.Out.WriteLine(factorial(StartingNumber));
+            Console.ReadLine();
+        }
+
+        static int factorial(int x)
+        {
+            Console.Out.WriteLine($"X is {x}");
+            if (x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * factorial(x - 1);
+            }
+        }
+    }
+}
